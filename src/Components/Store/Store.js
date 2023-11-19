3ad775fe8af4699f07b09494995bc11cb99d4952
parth thakor <parththakor2610@gmail.com>
2023-11-19 21:45:48 +0530

    fixing the bug user is logged in or not

--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Product from "./Product";
 import { useProducts } from "../../Context/ProductsContext";
-
-
+import { useAuthContext } from "../../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 
 function Store() {
-  const {products} = useProducts();
-
-  return   <div className="container text-center my-5">
-          <div className="row">
-
-      {
-        products.map((product)=>(
-          <Product key={product.id} product ={product}/>
-          ))
-        }
-        </div>
-  </div>
+  const { products } = useProducts();
+  const navigate = useNavigate();
+  const {userIsLoggedIn} = useAuthContext();
+   useEffect(()=>{
+     console.log(userIsLoggedIn);
+     if(!userIsLoggedIn){
+      navigate('/login')
+     }
+   })
+  return (
+    <div className="container text-center my-5">
+      <div className="row">
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Store;
--- src/Router/RouterProvider.js
+++ src/Router/RouterProvider.js
@@ -1,67 +1,103 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
 import App from "../App";
 import About from "../Components/About/About";
 import Home from "../Components/Home/Home";
 import Store from "../Components/Store/Store";
-import {  Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Contact from "../Components/ContactUs/Contact";
-import Details from  '../Components/Details/Deatils'
+import Details from "../Components/Details/Deatils";
 // import AuthForm from "./Components/Auth/AuthForm";
 import ForgotPass from "../Components/Auth/ForgotPassword";
 import Login from "../Components/Auth/Login";
 import Signup from "../Components/Auth/Signup";
-import { useAuthContext } from "../Context/AuthContext";
+
 function RoutesProvider() {
-  const {userIsLoggedIn} = useAuthContext();
-  console.log("From provider",userIsLoggedIn);
-  const router = createBrowserRouter([
-    {
-      path: "",
-      element:<App />,
-      children: [
-        {
-          path: "",
-          element: <Home/>,
-        },
-        {
-          path: "login",
-          element: <Login/>,
-        },
-        {
-          path: "resetpassword",
-          element: userIsLoggedIn ? <Navigate to="/login" /> : <ForgotPass/> 
-        },
-        {
-          path: "signup",
-          element: <Signup/>,
-        },
-        {
-          path: "about",
-          element: userIsLoggedIn ?  <About />:<Navigate to="/login" />,
-        },
-        {
-          path: "store",
-          element:userIsLoggedIn ?    <Store />:<Navigate to="/login" />,
-        },
-        {
-          path: "contact",
-          element:userIsLoggedIn ?   <Contact />:<Navigate to="/login" />,
-        },
-        {
-          path: "details/:id",
-          element: userIsLoggedIn ?  <Details/>:<Navigate to="/login" />,
-        },
-        {
-          path: "*",
-          element: <Login/>,
-        },
-       
-      ],
-    },
-  ]);
-  return (
-    <RouterProvider router={router}/>
-  )
+  const localToken = localStorage.getItem('token')
+     console.log(localToken);
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "",
+  //     element:<App />,
+  //     children: [
+  //       {
+  //         path: "",
+  //         element: <Home/>,
+  //       },
+  //       {
+  //         path: "login",
+  //         element: <Login/>,
+  //       },
+  //       {
+  //         path: "resetpassword",
+  //         element: userIsLoggedIn ? <Navigate to="/login" /> : <ForgotPass/>
+  //       },
+  //       {
+  //         path: "signup",
+  //         element: <Signup/>,
+  //       },
+  //       {
+  //         path: "about",
+  //         element: userIsLoggedIn ?  <About />:<Navigate to="/login" />,
+  //       },
+  //       {
+  //         path: "store",
+  //         element: userIsLoggedIn ?  <Navigate to="/login" /> :<Store />,
+  //       },
+  //       {
+  //         path: "contact",
+  //         element:userIsLoggedIn ?   <Contact />:<Navigate to="/login" />,
+  //       },
+  //       {
+  //         path: "details/:id",
+  //         element: userIsLoggedIn ?  <Details/>:<Navigate to="/login" />,
+  //       },
+  //       {
+  //         path: "*",
+  //         element: <Login/>,
+  //       },
+
+  //     ],
+  //   },
+  // ]);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />}>
+        <Route path="" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route
+          path="resetpassword"
+          element={localToken ? <ForgotPass /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="about"
+          element={localToken ? <About /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="store"
+          element={localToken ? <Store /> : <Navigate to="/login"  />}
+        />
+        <Route
+          path="contact"
+          element={localToken ? <Contact /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="details/:id"
+          element={localToken ? <Details /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default RoutesProvider;
