6381f245e065ffe3acb1e6e892ef9d1663df0b9b
parth thakor <parththakor2610@gmail.com>
2023-11-19 20:31:13 +0530

    user logout after the 5 minutes

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -16,9 +16,11 @@ export const useAuthContext = ()=>{
 
 const AuthContextProvider = (props)=>{
   const localToken = localStorage.getItem("token");
-
-  const [token ,setToken] = useState(null);
-  const [userIsLoggedIn ,setUserIsLoggedIn] = useState(localToken); // this is not reinitialzed so that we don't need worry about that // this is help full i don't idea onthis bro but remember this bro
+  const [token ,setToken] = useState(localToken);
+  const [userIsLoggedIn ,setUserIsLoggedIn] = useState(false); // this is not reinitialzed so that we don't need worry about that // this is help full i don't idea onthis bro but remember this bro
+console.log("this",userIsLoggedIn
+  )
+ 
 
  
 
@@ -27,6 +29,12 @@ const AuthContextProvider = (props)=>{
     console.log("setting  the tocke" , token)
     setToken(token);
     localStorage.setItem('token' , token);
+    // adding the timer to expire the token 
+
+    setTimeout(() => {
+      console.log("logout handler is called after the 5 minutes")
+        logoutHandler();
+    }, 5000 * 60);
     
   }
   const logoutHandler = ()=>{
@@ -35,6 +43,7 @@ const AuthContextProvider = (props)=>{
     setUserIsLoggedIn(false);
   }
   const onUserLogin = ()=>{
+    console.log("userlogin called")
     setUserIsLoggedIn(true);
   }
 
